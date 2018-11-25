8d99c9991e3ed68fbd6aaf567ab2b1b07db7387d
Allan Sims <allan@metsateave.ee>
2018-11-25 18:39:49 +0200

    Update setitup.sh

--- setitup.sh
+++ setitup.sh
@@ -83,7 +83,7 @@ cd ${srcDir}
 ncdfVer="4.6.2"
 wget -O netcdf-${ncdfVer}.tar.gz ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-c-4.6.2.tar.gz
 tar -xf netcdf-${ncdfVer}.tar.gz
-cd netcdf-${ncdfVer}
+cd netcdf-c-${ncdfVer}
 ./configure --prefix=${bldDir}
 make
 make install
