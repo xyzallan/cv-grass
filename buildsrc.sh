7c571492a60900d67364823e86be656e7eb01618
Allan Sims <allan@metsateave.ee>
2018-11-25 20:51:07 +0200

    Update buildsrc.sh

--- buildsrc.sh
+++ buildsrc.sh
@@ -4,6 +4,7 @@ rm -rf $srcDir
 mkdir -p $srcDir
 cd $srcDir
 
-yumdownloader --source proj --resolve
-yumdownloader --source gdal --resolve
+#yumdownloader --source proj --resolve
+#yumdownloader --source gdal --resolve
+yumdownloader --source netcdf --resolve
 
