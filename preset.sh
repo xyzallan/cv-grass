59d30ad72930236a4871369e97cd6226f2a181c1
Allan Sims <allan@metsateave.ee>
2018-11-25 19:53:43 +0200

    Update preset.sh

--- preset.sh
+++ preset.sh
@@ -4,8 +4,8 @@ wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -ivh epel-release-latest-7.noarch.rpm
 yum -y install gcc gcc-c++
 yum -y install byacc
-yum -y install proj-devel
-yum -y install geos-devel
-yum -y install zlib-devel
+#yum -y install proj-devel
+#yum -y install geos-devel
+#yum -y install zlib-devel
 
 rm -f *.rpm
