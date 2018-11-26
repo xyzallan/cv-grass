e42275e5b219b3fd36d2fe61190b7a5b0ce5a047
Allan Sims <allan@metsateave.ee>
2018-11-26 09:41:07 +0200

    Update preset.sh

--- preset.sh
+++ preset.sh
@@ -1,7 +1,7 @@
 # set up by root
 yum -y install wget
-wget -O /etc/yum.repos.d/grass74.repo https://copr.fedoraproject.org/coprs/neteler/grass74/repo/epel-7/neteler-grass74-epel-7.repo
-yum -y update
+#wget -O /etc/yum.repos.d/grass74.repo https://copr.fedoraproject.org/coprs/neteler/grass74/repo/epel-7/neteler-grass74-epel-7.repo
+#yum -y update
 wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -ivh epel-release-latest-7.noarch.rpm
 yum -y install gcc gcc-c++
