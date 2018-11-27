c2a52178c4ae8b9bec812e29d2f9a9089bed37cd
Allan Sims <allan@metsateave.ee>
2018-11-27 09:59:48 +0200

    Update setitup.sh

--- setitup.sh
+++ setitup.sh
@@ -25,74 +25,89 @@ projVer="4.8.0"
 if [ "${dwnl}" == "J" ]; then
    wget -O proj-${projVer}.tar.gz http://download.osgeo.org/proj/proj-${projVer}.tar.gz
 fi
-tar -xf proj-${projVer}.tar.gz
-srcPath=$(ls -1 | grep proj | grep -v "tar.gz")
-
 if [ "${inst}" == "J" ]; then
+   tar -xf proj-${projVer}.tar.gz
+   srcPath=$(ls -1 | grep proj | grep -v "tar.gz")
+
    cd $srcPath
    ./configure --prefix=${bldDir}
    make 
    make install
    make check || exit 1
 fi
-exit 1
 
 #===============================================================================
 cd ${srcDir}
 gdalVer="2.3.2"
-wget -O gdal-${gdalVer}.tar.gz http://download.osgeo.org/gdal/${gdalVer}/gdal-${gdalVer}.tar.gz 
-tar -xf gdal-${gdalVer}.tar.gz
-srcPath=$(ls -1 | grep gdal | grep -v "tar.gz")
-cd $srcPath
-./configure --prefix=${bldDir}
-make
-make install || exit 1
-
+if [ "${dwnl}" == "J" ]; then
+   wget -O gdal-${gdalVer}.tar.gz http://download.osgeo.org/gdal/${gdalVer}/gdal-${gdalVer}.tar.gz 
+fi
+if [ "${inst}" == "J" ]; then
+   tar -xf gdal-${gdalVer}.tar.gz
+   srcPath=$(ls -1 | grep gdal | grep -v "tar.gz")
+   cd $srcPath
+   ./configure --prefix=${bldDir}
+   make
+   make install || exit 1
+fi
 #===============================================================================
 cd ${srcDir}
 tiffVer="4.0.10"
-wget -O tiff-${tiffVer}.tar.gz https://download.osgeo.org/libtiff/tiff-${tiffVer}.tar.gz
-tar -xf tiff-${tiffVer}.tar.gz
-srcPath=$(ls -1 | grep tiff | grep -v "tar.gz")
-cd $srcPath
-./configure --prefix=${bldDir}
-make
-make install
-make check || exit 1
-
+if [ "${dwnl}" == "J" ]; then
+   wget -O tiff-${tiffVer}.tar.gz https://download.osgeo.org/libtiff/tiff-${tiffVer}.tar.gz
+fi
+if [ "${inst}" == "J" ]; then
+   tar -xf tiff-${tiffVer}.tar.gz
+   srcPath=$(ls -1 | grep tiff | grep -v "tar.gz")
+   cd $srcPath
+   ./configure --prefix=${bldDir}
+   make
+   make install
+   make check || exit 1
+fi
 #================================================================================
 cd ${srcDir}
 m4Ver="1.4.17"
-wget -O m4-${m4Ver}.tar.gz http://ftp.gnu.org/gnu/m4/m4-${m4Ver}.tar.gz
-tar -xf m4-${m4Ver}.tar.gz
-srcPath=$(ls -1 | grep m4 | grep -v "tar.gz")
-cd $srcPath
-./configure --prefix=${bldDir}
-make
-make install || exit 1
+if [ "${dwnl}" == "J" ]; then
+   wget -O m4-${m4Ver}.tar.gz http://ftp.gnu.org/gnu/m4/m4-${m4Ver}.tar.gz
+fi
+if [ "${inst}" == "J" ]; then
+   tar -xf m4-${m4Ver}.tar.gz
+   srcPath=$(ls -1 | grep m4 | grep -v "tar.gz")
+   cd $srcPath
+   ./configure --prefix=${bldDir}
+   make
+   make install || exit 1
+fi
 
 #================================================================================
 cd ${srcDir}
 flexVer="2.6.4"
-wget -O flex-${flexVer}.tar.gz https://github.com/westes/flex/releases/download/v${flexVer}/flex-${flexVer}.tar.gz
-tar -xf flex-${flexVer}.tar.gz
-srcPath=$(ls -1 | grep flex | grep -v "tar.gz")
-cd $srcPath
-./configure --prefix=${bldDir}
-make
-make install || exit 1
-
+if [ "${dwnl}" == "J" ]; then
+   wget -O flex-${flexVer}.tar.gz https://github.com/westes/flex/releases/download/v${flexVer}/flex-${flexVer}.tar.gz
+fi
+if [ "${inst}" == "J" ]; then
+   tar -xf flex-${flexVer}.tar.gz
+   srcPath=$(ls -1 | grep flex | grep -v "tar.gz")
+   cd $srcPath
+   ./configure --prefix=${bldDir}
+   make
+   make install || exit 1
+fi
 #================================================================================
 cd ${srcDir}
 geosVer="3.4.3"
-wget -O geos-${geosVer}.tar.bz2 http://download.osgeo.org/geos/geos-${geosVer}.tar.bz2
-tar -jxf geos-${geosVer}.tar.bz2
-srcPath=$(ls -1 | grep geos | grep -v "tar.bz2")
-cd $srcPath
-./configure --prefix=${bldDir}
-make
-make install || exit 1
-
+if [ "${dwnl}" == "J" ]; then
+   wget -O geos-${geosVer}.tar.bz2 http://download.osgeo.org/geos/geos-${geosVer}.tar.bz2
+fi
+if [ "${inst}" == "J" ]; then
+   tar -jxf geos-${geosVer}.tar.bz2
+   srcPath=$(ls -1 | grep geos | grep -v "tar.bz2")
+   cd $srcPath
+   ./configure --prefix=${bldDir}
+   make
+   make install || exit 1
+fi
 #================================================================================
 #cd ${srcDir}
 #ncdfVer="4.6.2"
@@ -108,38 +123,46 @@ make install || exit 1
 #================================================================================
 cd ${srcDir}
 zlibVer="1.2.11"
-wget -O zlib-${zlibVer}.tar.gz https://zlib.net/zlib-${zlibVer}.tar.gz
-tar -xf zlib-${zlibVer}.tar.gz
-srcPath=$(ls -1 | grep zlib | grep -v "tar.gz")
-cd $srcPath
-./configure --prefix=${bldDir}
-make
-make install || exit 1
-
+if [ "${dwnl}" == "J" ]; then
+   wget -O zlib-${zlibVer}.tar.gz https://zlib.net/zlib-${zlibVer}.tar.gz
+fi
+if [ "${inst}" == "J" ]; then
+   tar -xf zlib-${zlibVer}.tar.gz
+   srcPath=$(ls -1 | grep zlib | grep -v "tar.gz")
+   cd $srcPath
+   ./configure --prefix=${bldDir}
+   make
+   make install || exit 1
+fi
 #================================================================================
 cd ${srcDir}
 pythVer="2.7.15"
-wget -O Python-${pythVer}.tgz https://www.python.org/ftp/python/${pythVer}/Python-${pythVer}.tgz
-tar -xf Python-${pythVer}.tgz
-srcPath=$(ls -1 | grep Python | grep -v "tar.gz")
-cd $srcPath
-./configure --prefix=${bldDir}
-make
-make install || exit 1
-
+if [ "${dwnl}" == "J" ]; then
+   wget -O Python-${pythVer}.tgz https://www.python.org/ftp/python/${pythVer}/Python-${pythVer}.tgz
+fi
+if [ "${inst}" == "J" ]; then
+   tar -xf Python-${pythVer}.tgz
+   srcPath=$(ls -1 | grep Python | grep -v "tar.gz")
+   cd $srcPath
+   ./configure --prefix=${bldDir}
+   make
+   make install || exit 1
+fi
 #================================================================================
 cd ${srcDir}
 grasVer="7.4.2"
-wget -O grass-${grasVer}.tar.gz https://grass.osgeo.org/grass74/source/grass-${grasVer}.tar.gz
-tar -xf grass-${grasVer}.tar.gz
-srcPath=$(ls -1 | grep grass | grep -v "tar.gz")
-cd $srcPath
-sed -i 's/with_freetype=yes/with_freetype=no/g' configure
-sed -i 's/with_sqlite=yes/with_sqlite=no/g' configure
-./configure \
- --prefix=${bldDir} \
- --with-geos
- 
+if [ "${dwnl}" == "J" ]; then
+   wget -O grass-${grasVer}.tar.gz https://grass.osgeo.org/grass74/source/grass-${grasVer}.tar.gz
+fi
+if [ "${inst}" == "J" ]; then
+   tar -xf grass-${grasVer}.tar.gz
+   srcPath=$(ls -1 | grep grass | grep -v "tar.gz")
+   cd $srcPath
+   sed -i 's/with_freetype=yes/with_freetype=no/g' configure
+   sed -i 's/with_sqlite=yes/with_sqlite=no/g' configure
+   ./configure \
+    --prefix=${bldDir} \
+    --with-geos
  
 # --with-cxx \
 # --enable-largefile \
@@ -157,7 +180,8 @@ sed -i 's/with_sqlite=yes/with_sqlite=no/g' configure
 # --with-geos \
 # --with-netcdf \
 
-make
-make install || exit 1
+   make
+   make install || exit 1
+fi
 cd ${srcDir}
 export PATH=$oldPath
