60c1b492b76145e771e6e0c69825907dd0dd8046
Allan Sims <allan@metsateave.ee>
2020-03-13 11:25:54 +0200

    Create buildgdal.sh

---
+++ buildgdal.sh
@@ -0,0 +1,53 @@
+# How to make grass-gis build for CalValUs system (single tar.gz file) for CentOS7.
+# build made by user
+echo "Kas laadin alla?"
+read dwnl
+echo "Kas installeerin?"
+read inst
+srcDir=`pwd`/src
+bldDir=`pwd`/build
+oldPath=$PATH
+export PATH=$PATH:${bldDir}/bin:${bldDir}/include
+
+if [ "${inst}" == "J" ]; then
+   if [ -d "${bldDir}" ]; then
+      rm -rf ${bldDir}
+   fi
+fi
+if [ "${dwnl}" == "J" ]; then
+   if [ -d "${srcDir}" ]; then
+      rm -rf ${srcDir}
+   fi
+   mkdir -p ${srcDir}
+fi
+#===============================================================================
+cd ${srcDir}
+projVer="7.0.0"
+if [ "${dwnl}" == "J" ]; then
+   wget -O proj-${projVer}.tar.gz http://download.osgeo.org/proj/proj-${projVer}.tar.gz
+fi
+if [ "${inst}" == "J" ]; then
+   tar -xf proj-${projVer}.tar.gz
+   srcPath=$(ls -1 | grep proj | grep -v "tar.gz")
+
+   cd $srcPath
+   ./configure --prefix=${bldDir}
+   make 
+   make install
+   make check || exit 1
+fi
+
+#===============================================================================
+cd ${srcDir}
+gdalVer="3.0.4"
+if [ "${dwnl}" == "J" ]; then
+   wget -O gdal-${gdalVer}.tar.gz http://download.osgeo.org/gdal/${gdalVer}/gdal-${gdalVer}.tar.gz 
+fi
+if [ "${inst}" == "J" ]; then
+   tar -xf gdal-${gdalVer}.tar.gz
+   srcPath=$(ls -1 | grep gdal | grep -v "tar.gz")
+   cd $srcPath
+   ./configure --prefix=${bldDir}
+   make
+   make install || exit 1
+fi
